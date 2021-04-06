c2e67a9cc62addbee302409d02f14ecdb79ecfb7
timvvoodman <71102666+timvvoodman@users.noreply.github.com>
2021-04-06 13:19:21 -0400

    header done

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Typed from 'react-typed'
+
+function Header() {
+  return (
+    <div className="header-wrapper">
+      <div className="main-info">
+        <h1>Full Stack Developer</h1>
+        <Typed
+          className="typed-text"
+          strings={[
+            'MongoDB',
+            'Express.js',
+            'React',
+            'Node.js',
+            'SQL',
+            'MVC Design',
+          ]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <a href="" className="btn-main-offer">
+          Contact Me
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
